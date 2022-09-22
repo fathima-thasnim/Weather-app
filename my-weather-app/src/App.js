752d1b351d68792f0b6901afb12442c5e6e802d9
Fathima thasnim <thasnimfathima492@gmail.com>
2022-09-22 13:29:41 -0400

    add images

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import hotbg from './assets/hot.jpg'
+import coldbg from './assets/cold.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style= {{ backgroundImage: `url(${coldbg})`}}>
+      <div className="overlay">
+        <div className='container'>
+          <div className='section section__inputs'>
+            <input type="text" name="city" placeholder='Enter City...'/>
+            <button>°F</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
