32838ffe618e59ca3cc2c98b05a3b72bdbb4d18c
Fathima thasnim <thasnimfathima492@gmail.com>
2022-09-27 12:51:52 -0400

    add css

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -10,15 +10,15 @@ function App() {
             <input type="text" name="city" placeholder='Enter City...'/>
             <button>°F</button>
           </div>
-          <div className="section section__temparature"></div>
-          <div className="icon">
-            <h3>London, GB</h3>
-            <img src="https://www.pngall.com/wp-content/uploads/11/Weather-No-Background.png" width="55" height="50" alt="weathericon"/>
-            <h3>Cloudy</h3>
-          </div>
-          <div className="temparature">
-            <h1>34°C</h1>
-            <h3>what</h3>
+          <div className="section section__temparature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://www.pngall.com/wp-content/uploads/11/Weather-No-Background.png" width="55" height="50" alt="weathericon"/>
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temparature">
+              <h1>34 °C</h1>
+            </div>
           </div>
         </div>
       </div>
